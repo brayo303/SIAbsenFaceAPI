52ce97aedb02d5c9c7a2bff839c57a0c3b0d1d9a
brayo303 <thebeero303@gmail.com>
2022-01-06 00:36:42 +0700

    adding upload absen

--- upload.php
+++ upload.php
@@ -11,8 +11,17 @@ if( move_uploaded_file($_FILES['webcam']['tmp_name'],'upload/'.$filename) ){
 }
 
 $api = new API();
-echo $api->detectFace($url);
+$con = new Controller();
 
+$arrhasil=$api->detectFace($url);
+
+//var_dump($arrhasil);
+
+$id = $con->searchIdByMahasiswa($arrhasil['name'])['id'];
+
+$con->insertAbsen($arrhasil['name'],1,$id,1);
+
+echo 'cccc';
 
 
 
