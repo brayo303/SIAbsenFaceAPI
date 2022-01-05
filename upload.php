d0f4d2efbe039fc8600e7b6484e74190cb1b2384
brayo303 <thebeero303@gmail.com>
2022-01-05 22:06:00 +0700

    need upload fix

--- upload.php
+++ upload.php
@@ -3,13 +3,13 @@
 // new filename
 $filename = 'pic_'.date('YmdHis') . '.jpeg';
 
-$url = '';
+
 if( move_uploaded_file($_FILES['webcam']['tmp_name'],'upload/'.$filename) ){
-   $url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/upload/' . $filename;
+   $url = 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/upload/' . $filename;
 }
 
 
-// Return image url
+//$url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/upload/';
 echo $url;
 
 ?>
